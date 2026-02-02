e99a3d8ab12938bb5f7ab77afa58b579c8b49025
Rohit Pal <palrohitpr03@gmail.com>
2026-02-02 21:05:48 +0530

    annotations added

--- Rp-ecommerce/src/main/java/com/ecommerce/Rp_ecommerce/security/jwt/services/UserDetailsServiceImpl.java
+++ Rp-ecommerce/src/main/java/com/ecommerce/Rp_ecommerce/security/jwt/services/UserDetailsServiceImpl.java
@@ -7,15 +7,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
-public class UserDetailServiceImpl implements UserDetailsService { @Autowired
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService { @Autowired
    private UserRepository userRepository ;
     @Override
-    @Transactional()
+    @Transactional
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
         User user = userRepository.findByUsername(username).orElseThrow(() ->
-                new UsernameNotFoundException("User not found with username" + username));
+                new UsernameNotFoundException("User not found " ));
         return UserDetailsImpl.build(user);
     }
 }
