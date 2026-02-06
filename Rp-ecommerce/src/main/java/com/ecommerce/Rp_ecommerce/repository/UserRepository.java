a5334d4444ca8521c1c698c915e2e03552013ca0
Rohit Pal <palrohitpr03@gmail.com>
2026-02-07 00:00:43 +0530

    Repository method added

--- Rp-ecommerce/src/main/java/com/ecommerce/Rp_ecommerce/repository/UserRepository.java
+++ Rp-ecommerce/src/main/java/com/ecommerce/Rp_ecommerce/repository/UserRepository.java
@@ -7,4 +7,10 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
+
+    Optional<User> findByUsernameAndMobileNumber(String username, String mobileNumber);
+
+    boolean existsByUsernameAndMobileNumber(String username, String mobileNumber);
+
+    boolean existsByUsername(String testuser);
 }
